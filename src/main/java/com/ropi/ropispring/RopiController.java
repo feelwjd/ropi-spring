26171ec86694b59bb76c389a1e96613aeb641111
JeonJeongWook <jjwook324@gmail.com>
2022-01-12 15:40:35 +0900

    add main page

--- src/main/java/com/ropi/ropispring/RopiController.java
+++ src/main/java/com/ropi/ropispring/RopiController.java
@@ -1,14 +1,19 @@
 package com.ropi.ropispring;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
 public class RopiController {
-    @RequestMapping("/")
-    public List home(){
-        return null;
+	
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView home(){
+    	ModelAndView mv = new ModelAndView("main");
+    	System.out.println("home controller start");
+        return mv;
     }
 }
