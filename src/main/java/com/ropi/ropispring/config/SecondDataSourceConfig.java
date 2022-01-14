3a01f98e1c5e686d0b014a3a35388ef5bdddf374
JeonJeongWook <jjwook324@gmail.com>
2022-01-14 14:37:03 +0900

    add second DataSourceConfig

--- src/main/java/com/ropi/ropispring/config/SecondDataSourceConfig.java
+++ src/main/java/com/ropi/ropispring/config/SecondDataSourceConfig.java
@@ -1,39 +1,34 @@
 package com.ropi.ropispring.config;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import javax.sql.DataSource;
-
 @Configuration
-public class DataSourceConfig {
-	@Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
+public class SecondDataSourceConfig {
+    @ConfigurationProperties(prefix = "spring.second.datasource")
+    public DataSource secondDataSource() {
         return DataSourceBuilder.create().build();
     }
     
-	@Primary
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+    @Bean(name="secondSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(DataSource secondDataSource) throws Exception {
         SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setDataSource(secondDataSource);
         sessionFactoryBean.setMapperLocations(
                 new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml")
         );
         return sessionFactoryBean.getObject();
     }
 
-	@Primary
-    @Bean
+    @Bean(name="secondSqlSession")
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
