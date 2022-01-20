29d2283d986ac20f6ef2d86c41ce6f2c580e0d1d
JeonJeongWook <jjwook324@gmail.com>
2022-01-20 16:54:42 +0900

    Add csvReader

---
+++ src/main/java/com/ropi/ropispring/Controller/FileController.java
@@ -0,0 +1,5 @@
+package com.ropi.ropispring.Controller;
+
+public class FileController {
+
+}
---
+++ src/main/java/com/ropi/ropispring/util/CSVReader.java
@@ -0,0 +1,42 @@
+package com.ropi.ropispring.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVReader {
+	public List<List<String>> readCSV(String filePath) {
+        List<List<String>> csvList = new ArrayList<List<String>>();
+        File csv = new File(filePath);
+        BufferedReader br = null;
+        String line = "";
+
+        try {
+            br = new BufferedReader(new FileReader(csv));
+            while ((line = br.readLine()) != null) { // readLine()은 파일에서 개행된 한 줄의 데이터를 읽어온다.
+                List<String> aLine = new ArrayList<String>();
+                String[] lineArr = line.split(","); // 파일의 한 줄을 ,로 나누어 배열에 저장 후 리스트로 변환한다.
+                aLine = Arrays.asList(lineArr);
+                csvList.add(aLine);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) { 
+                    br.close(); // 사용 후 BufferedReader를 닫아준다.
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return csvList;
+    }
+}
