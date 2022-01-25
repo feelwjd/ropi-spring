6e82892bc99897243d31f566642a65d8767a5738
JeonJeongWook <jjwook324@gmail.com>
2022-01-25 09:02:22 +0900

    add Param annotaion

--- src/main/java/com/ropi/ropispring/DAO/SummaryDAO.java
+++ src/main/java/com/ropi/ropispring/DAO/SummaryDAO.java
@@ -2,6 +2,7 @@ package com.ropi.ropispring.DAO;
 
 import com.ropi.ropispring.Model.Summary;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public interface SummaryDAO {
     void setSummary(Summary summary); //추가
     void deleteSummary(Summary summary); //삭제
     void updateSummary(Summary summary); //수정
-    Summary getSummary(String symbol, String sectorcode);
+    Summary getSummary(@Param("symbol") String symbol, @Param("sectorcode") String sectorcode);
     int checkSummary(Summary summary);
     String dbCheck();
 }
