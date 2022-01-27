247b83e4084973565bb25d508e3893ef062e55cb
JeonJeongWook <jjwook324@gmail.com>
2022-01-27 12:50:30 +0900

    Fix DB ssh connect

--- src/main/java/com/ropi/ropispring/RopiSpringApplication.java
+++ src/main/java/com/ropi/ropispring/RopiSpringApplication.java
@@ -3,10 +3,20 @@ package com.ropi.ropispring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ropi.ropispring.config.SSHConnection;
+
 @SpringBootApplication
 public class RopiSpringApplication {
+	private static SSHConnection ssh;
+	public RopiSpringApplication() {
+		System.out.println("ropiSpringApplication check");
+		
+		ssh = new SSHConnection();
+		
+	}
 
     public static void main(String[] args) {
+    	System.out.println("main start");
         SpringApplication.run(RopiSpringApplication.class, args);
     }
 
--- src/main/java/com/ropi/ropispring/config/SSHConnection.java
+++ src/main/java/com/ropi/ropispring/config/SSHConnection.java
@@ -1,36 +1,42 @@
 package com.ropi.ropispring.config;
 import java.util.Properties;
+import java.util.function.Consumer;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
 public class SSHConnection {
-	private final static String HOST = "192.168.0.201";
-	private final static Integer PORT = 22;
-	private final static String SSH_USER = "htone";
-	private final static String SSH_PW = "htone123";
+	private static JSch jsch = new JSch();
 
-	private Session session;
-	
-	public void closeSSH() {
-		session.disconnect();
-	}
-	
-	public SSHConnection() {
-		try {
-			System.out.println("check");
-			Properties config = new Properties();
-			config.put("StrictHostKeyChecking", "no");
-			JSch jsch = new JSch();
-			session = jsch.getSession(SSH_USER, HOST, PORT);
-			session.setPassword(SSH_PW);
-			session.setConfig(config);
-			session.connect();
-			session.setPortForwardingL(3306, "127.0.0.1", 3306);
-		} catch (JSchException e) {
-			e.printStackTrace();
-		}
-		
-	}
+    private String url = "128.134.37.244";
+    private String username = "ropi";
+    private String password = "ropi123";
+    private int port = 22022;  //ssh포트
+    private int lport = 26016;  //원격 접속 후 가상으로 포워딩할 포트
+    private int rport = 3306;  //실제 사용할 데이터베이스 포트
+
+    private Session session;
+
+    public SSHConnection init(Consumer<Boolean> arg) {  
+        try {
+            session = jsch.getSession(username, url, port);
+            session.setPassword(password);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            session.setPortForwardingL(lport, "127.0.0.1", rport);		
+            arg.accept(true);
+        } catch (Exception e) {
+            arg.accept(false);	
+        }
+        return this;
+    }
+
+    public void shutdown() throws Exception {
+        if (session != null && session.isConnected()) {
+            session.disconnect();
+        }
+    }
 }
