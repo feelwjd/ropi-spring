13c8ede437992d44de729c019327a8e3419592a7
JeonJeongWook <jjwook324@gmail.com>
2022-02-08 17:04:41 +0900

    getbootstrap > bootstrapk

--- src/main/java/com/ropi/ropispring/Controller/SummaryController.java
+++ src/main/java/com/ropi/ropispring/Controller/SummaryController.java
@@ -205,45 +205,45 @@ public class SummaryController {
 		return mv;
 	}
 
-	@GetMapping(value = "/delete/{db}/{select}/{countrycode}")
-	public ModelAndView selectDelete(@PathVariable("db")String db,@PathVariable("select")String select, @PathVariable("countrycode")String ctc, ModelAndView mv){
-		String[] arrSymbol = select.toString().split(",");
-		String[] arrCountrycode = ctc.toString().split(",");
-		Summary summary = new Summary();
-		try	{
-			for (int i =0;i<arrSymbol.length; i++){
-				String symbol = arrSymbol[i];
-				String countrycode = arrCountrycode[i];
-				switch (db){
-					case "ropi1":
-						logger.info("server 1 delete");
-						try {
-							summary = summaryService.getSummary(symbol, countrycode);
-							summaryService.deleteSummary(summary);
-							break;
-						}catch (Exception e){}
-
-					case "ropi6":
-						logger.info("server 2 delete");
-						try {
-							summary = summaryService.getRopi6Summary(symbol, countrycode);
-							summaryService.deleteRopi6Summary(summary);
-							break;
-						}catch (Exception e){}
-
-					case "ropi7":
-						logger.info("server 3 delete");
-						try {
-							summary = summaryService.getRopi7Summary(symbol, countrycode);
-							summaryService.deleteRopi7Summary(summary);
-							break;
-						}catch (Exception e){}
-				}
-			}
-			mv.setViewName("redirect:/");
-		}catch (Exception e){}
-		return mv;
-	}
+//	@GetMapping(value = "/delete/{db}/{select}/{countrycode}")
+//	public ModelAndView selectDelete(@PathVariable("db")String db,@PathVariable("select")String select, @PathVariable("countrycode")String ctc, ModelAndView mv){
+//		String[] arrSymbol = select.toString().split(",");
+//		String[] arrCountrycode = ctc.toString().split(",");
+//		Summary summary = new Summary();
+//		try	{
+//			for (int i =0;i<arrSymbol.length; i++){
+//				String symbol = arrSymbol[i];
+//				String countrycode = arrCountrycode[i];
+//				switch (db){
+//					case "ropi1":
+//						logger.info("server 1 delete");
+//						try {
+//							summary = summaryService.getSummary(symbol, countrycode);
+//							summaryService.deleteSummary(summary);
+//							break;
+//						}catch (Exception e){}
+//
+//					case "ropi6":
+//						logger.info("server 2 delete");
+//						try {
+//							summary = summaryService.getRopi6Summary(symbol, countrycode);
+//							summaryService.deleteRopi6Summary(summary);
+//							break;
+//						}catch (Exception e){}
+//
+//					case "ropi7":
+//						logger.info("server 3 delete");
+//						try {
+//							summary = summaryService.getRopi7Summary(symbol, countrycode);
+//							summaryService.deleteRopi7Summary(summary);
+//							break;
+//						}catch (Exception e){}
+//				}
+//			}
+//			mv.setViewName("redirect:/");
+//		}catch (Exception e){}
+//		return mv;
+//	}
 	
 	@PostMapping(value = "/copySummary/{database}")
 	public ModelAndView copySummary(@PathVariable("database")String database, @ModelAttribute Summary summary, @RequestParam("selectedDB")String selectedDB) {
