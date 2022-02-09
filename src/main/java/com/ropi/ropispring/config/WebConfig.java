3950f4ee5c1145249805ef99376661cbc797a587
JeonJeongWook <jjwook324@gmail.com>
2022-02-09 10:38:40 +0900

    추가 js 외부 호출

--- src/main/java/com/ropi/ropispring/config/WebConfig.java
+++ src/main/java/com/ropi/ropispring/config/WebConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.ropi.ropispring.Controller" })
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
    private final int MAX_SIZE = 20 * 1024 * 1024;
    
    @Bean
@@ -23,4 +25,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
       multipartResolver.setMaxInMemorySize(0);
       return multipartResolver;
    }
+   
+   @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+//		WebMvcConfigurer.super.addResourceHandlers(registry);
+	   registry.addResourceHandler("/**")
+	   			.addResourceLocations("classpath:/templates/", "classpath:/static/");
+	}
 }
