9aa8554c232c6bb159b761b6c2a46b4ac6036851
JeonJeongWook <jjwook324@gmail.com>
2022-02-10 16:58:02 +0900

    reset masetr branch

--- src/main/java/com/ropi/ropispring/config/ssh/MyContextListener.java
+++ src/main/java/com/ropi/ropispring/config/ssh/MyContextListener.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyContextListener implements ServletContextListener{
-	private Ropi1SSH ssh1Connection;
-	private Ropi6SSH ssh6Connection;
-	private Ropi7SSH ssh7Connection;
+//	private Ropi1SSH ssh1Connection;
+//	private Ropi6SSH ssh6Connection;
+//	private Ropi7SSH ssh7Connection;
 	
 	public MyContextListener() {
 		super();
@@ -19,9 +19,9 @@ public class MyContextListener implements ServletContextListener{
 	public void contextInitialized(ServletContextEvent sce) {
 		System.out.println("context initialzed");
 		try {
-			ssh1Connection = new Ropi1SSH();
-			ssh6Connection = new Ropi6SSH();
-			ssh7Connection = new Ropi7SSH();
+//			ssh1Connection = new Ropi1SSH();
+//			ssh6Connection = new Ropi6SSH();
+//			ssh7Connection = new Ropi7SSH();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -30,8 +30,8 @@ public class MyContextListener implements ServletContextListener{
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		System.out.println("init destory!");
-		ssh1Connection.closeSSH();
-		ssh6Connection.closeSSH();
-		ssh7Connection.closeSSH();
+//		ssh1Connection.closeSSH();
+//		ssh6Connection.closeSSH();
+//		ssh7Connection.closeSSH();
 	}
 }
