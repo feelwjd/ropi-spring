ff9f74f73b56bcd23ac9aa16fd71bfa0b6232d76
JeonJeongWook <jjwook324@gmail.com>
2022-02-11 10:14:06 +0900

    로컬 세팅

---
+++ src/main/resources/static/js/summaryDetail.js
@@ -0,0 +1,28 @@
+function deleteSummary(){
+    	if(confirm("삭제하시겠습니까?")){
+    		var database = $("#database").val();
+    		$("#summaryForm").attr("action", "/" + database + "/deleteSummary");
+    		$("#summaryForm").submit();
+    	}
+    }
+    
+    function updateSummary(){
+    	$("#summaryForm").attr("action", "update")
+    }
+    
+    function copySummary(){
+    	var checkArray = new Array();
+    	
+    	$("input[name='inputCheck[]']:checked").each(function(){
+    		var tmpVal = $(this).val();
+    		checkArray.push(tmpVal);
+    	});
+    	
+    	if(checkArray.length < 1 ){
+   	    	alert("값을 선택해주시기 바랍니다.");
+   	    	return;
+   	    }else{
+   	    	$("#selectedDB").val(checkArray);
+   	    	$("#copyForm").submit();
+   	    }
+    }
